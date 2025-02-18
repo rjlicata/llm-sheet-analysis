bf753b88236cbbda6bd8f87cfa3117f71b56f928
rjlicata <rjlicata@mix.wvu.edu>
2025-02-17 21:52:46 -0700

    finalized README

--- src/sheet_analysis/llm/prompts.py
+++ src/sheet_analysis/llm/prompts.py
@@ -8,7 +8,7 @@ Your code should contain only the imports, the function (func) that takes one ar
 You can assume that the data will always be a pandas DataFrame. \
 Please do not include any print statements or test/sample data in your code. \
 If your code requires figures, use the matplotlib `savefig` function to save it to the following path: "figures/output.png". \
-Remember to close the figure before the end of the function.
+Remember to close the figure before the end of the function. Only generate figures when prompted to.
 The only packages you can import are: pandas, numpy, datetime, re, sklearn, matplotlib, and scipy.
 
 Example 1:
